ca9f5c33a49fd8a9c47f6eebed922f669c4ad3d9
Przemek <przemek.kocjan96@gmail.com>
2021-04-12 22:16:43 +0200

    Solved

---
+++ FrogJmp.c
@@ -0,0 +1,10 @@
+// you can write to stdout for debugging purposes, e.g.
+// printf("this is a debug message\n");
+
+int solution(int X, int Y, int D) {
+    // write your code in C99 (gcc 6.2.0)
+    if((Y-X)%D==0)
+        return (Y-X)/D;
+    else
+        return (Y-X)/D+1;
+}
